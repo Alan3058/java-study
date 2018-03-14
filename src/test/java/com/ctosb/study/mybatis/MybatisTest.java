08196c6c809777de0880223e615538ddb222884f
李连钢 <lilg1163@13322ty.com>
2018-03-14 17:24:09 +0800

    添加mybatis测试

--- src/test/java/com/ctosb/study/mybatis/MybatisTest.java
+++ src/test/java/com/ctosb/study/mybatis/MybatisTest.java
@@ -3,12 +3,14 @@ package com.ctosb.study.mybatis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.ctosb.study.mybatis.mapper.UserMapper;
@@ -22,14 +24,41 @@ import com.ctosb.study.mybatis.model.User;
  */
 public class MybatisTest {
 
-	@Test
-	public void testMybatis() throws IOException {
+	private SqlSessionFactory sqlSessionFactory;
+	private SqlSession sqlSession;
+
+	@Before
+	public void before() throws IOException {
 		InputStream inputStream = Resources.getResourceAsStream("com/ctosb/study/mybatis/mybatis-config.xml");
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		SqlSession sqlSession = sqlSessionFactory.openSession();
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		sqlSession = sqlSessionFactory.openSession();
+		testInsert();
+	}
+
+	@Test
+	public void testInsert() throws IOException {
 		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 		int result = mapper.insert(new User().setUserName("test").setPassword("123456"));
 		sqlSession.commit();
 		Assert.assertTrue(result > 0);
+	}
+
+	@Test
+	public void testCache() throws IOException {
+		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+		// 测试缓存
+		List<User> users = mapper.getByUserName("test");
+		// 这里不会发起sql
+		users = mapper.getByUserName("test");
+		Assert.assertTrue(users.size() > 0);
+	}
+
+	@Test
+	public void testAnnotation() throws IOException {
+		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+		// 测试缓存
+		List<User> users = mapper.selectAll();
+		// 这里不会发起sql
+		Assert.assertTrue(users.size() > 0);
 	}
 }
--- src/test/java/com/ctosb/study/mybatis/mapper/UserMapper.java
+++ src/test/java/com/ctosb/study/mybatis/mapper/UserMapper.java
@@ -3,12 +3,12 @@ package com.ctosb.study.mybatis.mapper;
 import java.util.List;
 
 import com.ctosb.study.mybatis.model.User;
+import org.apache.ibatis.annotations.Select;
 
 
 public interface UserMapper {
 
 	int insert(User user);
-
 	int insertAll(List<User> users);
 
 	int delete(int id);
@@ -16,5 +16,8 @@ public interface UserMapper {
 	int deleteByUserName(String userName);
 
 	List<User> getByUserName(String userName);
+
+	@Select(value = "select * from user")
+	List<User> selectAll();
 
 }
