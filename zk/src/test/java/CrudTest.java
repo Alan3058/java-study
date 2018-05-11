ad76278d56e1123ce5bc27f4d09eca123f28b5d5
alan <ylxy3058>
2018-05-12 01:23:14 +0800

    增加查询和删除修改方法

--- zk/src/test/java/CrudTest.java
+++ zk/src/test/java/CrudTest.java
@@ -1,14 +1,12 @@
+import java.util.List;
+
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryForever;
-import org.apache.curator.retry.RetryOneTime;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * crud test
@@ -18,23 +16,28 @@ import java.util.List;
  */
 public class CrudTest {
 
-	CuratorFramework client;
-	String connection = "192.168.10.44:2182";
-	int timeout = 5000;
+    CuratorFramework client;
+    String connection = "192.168.10.44:2182";
+    int timeout = 5000;
 
-	@Before
-	public void before() {
-		client = CuratorFrameworkFactory.builder().connectString(connection).retryPolicy(new RetryForever(1000))
-				.connectionTimeoutMs(timeout).sessionTimeoutMs(timeout).build();
-	}
+    @Before
+    public void before() {
+        client = CuratorFrameworkFactory.builder().connectString(connection).retryPolicy(new RetryForever(1000))
+                .connectionTimeoutMs(timeout).sessionTimeoutMs(timeout).build();
+    }
 
-	@Test
-	public void crud() throws Exception {
-		client.start();
-		List<String> nodes = client.getChildren().forPath("/");
-		System.out.println(String.join(",", nodes));
-		String test = client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath("/curator/3",
-				"abc".getBytes());
-		System.out.println(test);
-	}
+    @Test
+    public void crud() throws Exception {
+        client.start();
+        List<String> nodes = client.getChildren().forPath("/");
+        System.out.println("query all nodes:" + String.join(",", nodes));
+        String path = "/curator/3";
+        String test = client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path,
+                "abc".getBytes());
+        System.out.println("create success node:" + test + ",version:"+client.checkExists().forPath(path).getVersion());
+        Stat stat = client.setData().forPath(path, test.getBytes());
+        System.out.println("update data, stat:" + stat+",version:"+stat.getVersion());
+        client.delete().forPath(path);
+        System.out.println("success delete.exist:" + (client.checkExists().forPath(path) != null));
+    }
 }
