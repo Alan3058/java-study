dd94ec89f5848b86abef2e6f27946aa541e29099
alan <ylxy3058>
2018-05-12 09:47:08 +0800

    修改测试类

--- zk/src/test/java/CrudTest.java
+++ zk/src/test/java/CrudTest.java
@@ -16,28 +16,32 @@ import org.junit.Test;
  */
 public class CrudTest {
 
-    CuratorFramework client;
-    String connection = "192.168.10.44:2182";
-    int timeout = 5000;
+	CuratorFramework client;
+	String connection = "localhost:2181";
+	int timeout = 5000;
 
-    @Before
-    public void before() {
-        client = CuratorFrameworkFactory.builder().connectString(connection).retryPolicy(new RetryForever(1000))
-                .connectionTimeoutMs(timeout).sessionTimeoutMs(timeout).build();
-    }
+	@Before
+	public void before() {
+		client = CuratorFrameworkFactory.builder().connectString(connection).retryPolicy(new RetryForever(1000))
+				.connectionTimeoutMs(timeout).sessionTimeoutMs(timeout).build();
+	}
 
-    @Test
-    public void crud() throws Exception {
-        client.start();
-        List<String> nodes = client.getChildren().forPath("/");
-        System.out.println("query all nodes:" + String.join(",", nodes));
-        String path = "/curator/3";
-        String test = client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path,
-                "abc".getBytes());
-        System.out.println("create success node:" + test + ",version:"+client.checkExists().forPath(path).getVersion());
-        Stat stat = client.setData().forPath(path, test.getBytes());
-        System.out.println("update data, stat:" + stat+",version:"+stat.getVersion());
-        client.delete().forPath(path);
-        System.out.println("success delete.exist:" + (client.checkExists().forPath(path) != null));
-    }
+	@Test
+	public void crud() throws Exception {
+		client.start();
+		List<String> nodes = client.getChildren().forPath("/");
+		System.out.println("query all nodes:" + String.join(",", nodes));
+		String path = "/curator/3";
+		String test = client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path,
+				"abc".getBytes());
+		Stat stat = client.checkExists().forPath(path);
+		assert (stat.getVersion() == 0);
+		System.out.println("create success node:" + test + ",version:" + stat.getVersion());
+		stat = client.setData().forPath(path, test.getBytes());
+		assert (stat.getVersion() == 1);
+		System.out.println("update data, stat:" + stat + ",version:" + stat.getVersion());
+		client.delete().forPath(path);
+		stat = client.checkExists().forPath(path);
+		System.out.println("success delete.exist:" + (stat != null));
+	}
 }
